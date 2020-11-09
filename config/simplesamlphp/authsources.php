23f89fa727680ae81398763a746bc1c2ca54a03a
Alex Moore <alex@actioniq.com>
2020-11-09 13:34:51 -0500

    update user info

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -9,14 +9,14 @@ $config = array(
     'example-userpass' => array(
         'exampleauth:UserPass',
         'user1:user1pass' => array(
-            'uid' => array('1'),
-            'eduPersonAffiliation' => array('group1'),
             'email' => 'user1@example.com',
+            'first_name' => 'Roo',
+            'family_name' => 'Sato',
         ),
         'user2:user2pass' => array(
-            'uid' => array('2'),
-            'eduPersonAffiliation' => array('group2'),
             'email' => 'user2@example.com',
+            'first_name' => 'Java',
+            'family_name' => 'Mutt',
         ),
     ),
 
